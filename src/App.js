276e7783a1cef42b7d6e2dc11038e481e8494f26
Ramiro <ramiroprogram6@gmail.com>
2023-11-16 16:48:38 -0300

    rama grilla y buscador recien creada y funcionando

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h2>Tu trabajo es encargarte de todo el nav con su buscador y demás</h2>
+      <h2>también te encargas de crear la grilla la página principal</h2>
+      <h3>por cierto, no trabajes con código en app.js</h3>
+      <h1>NO OLVIDES DE BORRAR TODO ESTO EN APP.JS</h1>
     </div>
   );
 }
