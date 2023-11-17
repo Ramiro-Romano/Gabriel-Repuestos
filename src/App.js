521c0d60ad57eb0673813fcad293c1fb124bdc42
Ramiro-Romano <ramiroprogram6@gmail.com>
2023-11-17 18:04:24 -0300

    primer commit borrando las etiquetas H

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Tu trabajo es dedicarte a todo el footer</h2>
-      <h2>donde se encuentran los inputs y botones</h2>
-      <h3>por cierto, no trabajes con código en app.js</h3>
-      <h1>NO OLVIDES DE BORRAR TODO ESTO EN APP.JS</h1>
+     
     </div>
   );
 }
