3729bd0ca952f2a39fe9ad27d0a0bb96f3d44432
Fede <fede829@gmail.com>
2023-11-23 17:51:29 -0300

    grilla y buscador terminado

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Grilla from '../Components/Grilla/Grilla';
 import Buscador from '../Components/Buscador/Buscador';
+import axios from 'axios';
 
 const Home = () => {
   const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/productos'); // Ajusta la URL según tu configuración de servidor
+        setProductos(response.data);
+      } catch (error) {
+        console.error('Error al obtener datos:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Este efecto se ejecuta solo una vez al cargar la página
 
   const handleSearch = (result) => {
     setProductos(result);
